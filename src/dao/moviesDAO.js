a7e7d589a8cee93c8d4b7d942f4a0ecc9ec7c59e
mich <mich-cook@users.noreply.github.com>
2020-02-17 01:14:15 -0800

    Chapter 2 - Ticket: Paging

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -236,18 +236,7 @@ export default class MoviesDAO {
       return { moviesList: [], totalNumMovies: 0 }
     }
 
-    /**
-    Ticket: Paging
-
-    Before this method returns back to the API, use the "moviesPerPage" and
-    "page" arguments to decide the movies to display.
-
-    Paging can be implemented by using the skip() and limit() cursor methods.
-    */
-
-    // TODO Ticket: Paging
-    // Use the cursor to only return the movies that belong on the current page
-    const displayCursor = cursor.limit(moviesPerPage)
+    const displayCursor = cursor.skip(moviesPerPage * page).limit(moviesPerPage)
 
     try {
       const moviesList = await displayCursor.toArray()
