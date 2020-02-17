cdfc96c1f2131921a77e26ff87205bd5585ba84b
mich <mich-cook@users.noreply.github.com>
2020-02-17 00:04:05 -0800

    projection lab cleanup (can be clean and complete ... )

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -44,23 +44,8 @@ export default class MoviesDAO {
    * @returns {Promise<CountryResult>} A promise that will resolve to a list of CountryResults.
    */
   static async getMoviesByCountry(countries) {
-    /**
-    Ticket: Projection
-
-    Write a query that matches movies with the countries in the "countries"
-    list, but only returns the title and _id of each movie.
-
-    Remember that in MongoDB, the $in operator can be used with a list to
-    match one or more values of a specific field.
-    */
-
     let cursor
     try {
-      // TODO Ticket: Projection
-      // Find movies matching the "countries" list, but only return the title
-      // and _id. Do not put a limit in your own implementation, the limit
-      // here is only included to avoid sending 46000 documents down the
-      // wire.
       cursor = await movies.find(
         { countries: { $in: countries } },
         { projection: { title: 1 } },
