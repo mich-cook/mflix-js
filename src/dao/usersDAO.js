8209e0dbb7795a7effff11f43aeeaee85cec0907
mich <mich-cook@users.noreply.github.com>
2020-02-19 00:48:56 -0800

    Chapter 2 - Ticket: Durable Writes

--- src/dao/usersDAO.js
+++ src/dao/usersDAO.js
@@ -37,9 +37,7 @@ export default class UsersDAO {
     */
 
     try {
-      // TODO Ticket: Durable Writes
-      // Use a more durable Write Concern for this operation.
-      await users.insertOne({ ...userInfo })
+      await users.insertOne({ ...userInfo }, { w: "majority" })
       return { success: true }
     } catch (e) {
       if (String(e).startsWith("MongoError: E11000 duplicate key error")) {
